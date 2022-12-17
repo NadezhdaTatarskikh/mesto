3351ef94683f11f17884580ad886d3737af764b1
N.Tatarskikh <nadya-garifyanova@yandex.ru>
2022-12-17 14:29:58 +0300

    Внесены изменения2

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
-const popupOpened = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__profile');
-const nameInput = document.querySelector('.popup__input_text-name');
-const jobinput = document.querySelector('.popup__input_text-job');
+const nameInput = document.querySelector('.popup__input_text_name');
+const jobinput = document.querySelector('.popup__input_text_job');
 const editProfile = document.querySelector('.profile__edit');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popupClose = document.querySelector('.popup__close-button');
 
-function openPopup(popup) {
-    popupOpened.classList.add('popup_opened');
-}
 function openPopup() {
-    popupOpened.classList.add("popup_opened");
+    popup.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobinput.value = profileJob.textContent;
 }
 editProfile.addEventListener("click", openPopup);
 
 function closePopup() {
-    popupOpened.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 popupClose.addEventListener("click", closePopup);
 
@@ -26,7 +23,7 @@ function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobinput.value;
-    closePopup(openPopup);
+    closePopup();
 };
 
 formElement.addEventListener('submit',  handleFormSubmit);
